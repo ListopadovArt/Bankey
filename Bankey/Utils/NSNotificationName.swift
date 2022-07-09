a6c1ac3036eddb88dad76e14f2d5a6d5f12a728e
Artem Listopadov <artbdarm9@mail.ru>
2022-07-09 21:38:16 +0300

    feat: add logout button

---
+++ Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Artem Listopadov on 9.07.22.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
